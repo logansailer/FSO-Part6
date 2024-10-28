6f6903440cdbe6ce9929456676c46a6a5f88b87e
Logan <logansailer0633@gamil.com>
2024-10-28 00:40:13 -0400

    finished communicating with server

--- part6/src/reducers/noteReducer.js
+++ part6/src/reducers/noteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import noteService from "../services/notes";
 
 const initialState = [];
 
@@ -6,9 +7,6 @@ const noteSlice = createSlice({
   name: "notes",
   initialState,
   reducers: {
-    createNote(state, action) {
-      state.push(action.payload);
-    },
     toggleImportanceOf(state, action) {
       const id = action.payload;
       const noteToChange = state.find((n) => n.id === id);
@@ -23,10 +21,25 @@ const noteSlice = createSlice({
       state.push(action.payload);
     },
     setNotes(state, action) {
-      return action.payload
-    }
+      return action.payload;
+    },
   },
 });
 
-export const { createNote, toggleImportanceOf, appendNote, setNotes } = noteSlice.actions;
+export const { toggleImportanceOf, appendNote, setNotes } = noteSlice.actions;
+
+export const initializeNotes = () => {
+  return async (dispatch) => {
+    const notes = await noteService.getAll();
+    dispatch(setNotes(notes));
+  };
+};
+
+export const createNote = (content) => {
+  return async (dispatch) => {
+    const newNote = await noteService.createNew(content);
+    dispatch(appendNote(newNote));
+  };
+};
+
 export default noteSlice.reducer;
